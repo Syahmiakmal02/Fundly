058c128d58939898552adde7072ec258b111bec4
kemallio <D20221101856@siswa.upsi.edu.my>
2025-01-09 23:47:09 +0800

    css switch

--- auth/login.php
+++ auth/login.php
@@ -46,6 +46,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/auth.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="shortcut icon" href="/imgs/logo.png" type="image/x-icon">
+
 </head>
 
 <body>
